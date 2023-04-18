0ecf156aef3b98db38ce90aef5d73c62968ca81b
Ma Joonyoung <joonyoung1023@gmail.com>
2023-04-18 22:30:41 +0900

    Create example1.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtCore import QUrl
-from PyQt6.QtWidgets import QApplication
-from PyQt6.QtWebEngineWidgets import QWebEngineView
-
-app = QApplication([])
-view = QWebEngineView()
-view.load(QUrl.fromLocalFile('./HW_2/20191579_객체지향분석및설계_HW2.pdf'))
-view.show()
-app.exec()
