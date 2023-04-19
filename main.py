b2ee771209a9fca2163d1ed9b919ff9d53a37f59
Pilmo <pilmokim99@gmail.com>
2023-04-19 14:59:20 +0900

    기존의 코드는 작동이 안됐지만 이 코드는 작동은 합니다. 다만 파일을 정상적으로 불러와서 제어할 수는 없어요 ㅠ

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from PyQt6.QtCore import QUrl
-from PyQt6.QtWidgets import QApplication
-from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtPrintSupport import QPdfDocument, QPdfView
 
 app = QApplication([])
-view = QWebEngineView()
-view.load(QUrl.fromLocalFile('./HW_2/20191579_객체지향분석및설계_HW2.pdf'))
-view.show()
-app.exec()
+window = QMainWindow()
+pdf_doc = QPdfDocument()
+pdf_doc.load('./HW_2/20191579_객체지향분석및설계_HW2.pdf')
+pdf_view = QPdfView(pdf_doc)
+pdf_view.setRenderHint(QPainter.Antialiasing, True)
+window.setCentralWidget(pdf_view)
+window.show()
+app.exec_()
