871566ddcedfecf9941786c1ad79339bf1e7a02d
joonyoung1 <joonyoung1023@gmail.com>
2023-05-08 20:27:53 +0900

    :sparkles: note 기능 추가
    사용자가 하단에 작성한 내용이 pdf와 함께 저장됩니다. File 메뉴의 Save
    기능으로 변경사항들을 저장할 수 있습니다.

--- src/note_widget.py
+++ src/note_widget.py
@@ -7,23 +7,41 @@ class NoteWidget(QTextEdit):
     def __init__(self):
         super().__init__()
         self.setFocusPolicy(Qt.ClickFocus)
+        self.pages = []
+        self.annots = []
         self.notes = []
         self.idx = 0
-    
-    def load_notes(self, pdf):
-        for page in pdf:
-            for annot in page.annots():
-                if annot.info['title'] == 'SsamGPT':
-                    self.notes.append(annot.info['content'])
-                    break
-            else:
-                self.notes.append('')
-        self.set_idx(0)
-    
+
     def set_idx(self, idx):
         self.idx = idx
         self.setText(self.notes[idx])
     
+    def load_notes(self, pdf):
+        for page in pdf:
+            self.pages.append(page)
+            for annot in page.annots():
+                if annot.info['title'] == 'SsamGPT':
+                    self.notes.append(annot.info['content'])
+                    self.annots.append(annot)
+                    break
+            else:
+                self.notes.append('')
+                self.annots.append(None)
+        self.set_idx(0)
+    
     def update_note(self):
         self.notes[self.idx] = self.toPlainText()
         self.clearFocus()
+
+    def save_note(self):
+        for i, page in enumerate(self.pages):
+            if self.notes[i] != '':
+                if self.annots[i]:
+                    self.annots[i].set_info(content=self.notes[i])
+                else:
+                    annot = page.add_text_annot((-20, -20), self.notes[i])
+                    annot.set_info(title='SsamGPT')
+            elif self.annots[i]:
+                page.delete_annot(self.annots[i])
+        
+            
