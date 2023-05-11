e00a62997b92ca93a4e82f79ef8e4caa65a6c663
joonyoung1 <joonyoung1023@gmail.com>
2023-05-12 08:40:36 +0900

    :lipstick: ChatWidget의 UI 업데이트
    텍스트의 길이가 길이지면 적당하게 여러 줄로 나누어 표시합니다. 질문과
    답변을 구분하여 오른쪽 또는 왼쪽에 붙이고 색상을 구별할 수 있습니다.

--- src/chat_widget.py
+++ src/chat_widget.py
@@ -1,3 +1,4 @@
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 
@@ -5,27 +6,42 @@ class ChatWidget(QWidget):
     
     def __init__(self):
         super().__init__()
-
         self.history_box = QScrollArea()
-        history_widget = QWidget()
-        history_layout = QGridLayout()
-        history_layout.setSizeConstraint(QLayout.SetDefaultConstraint)
-        for i in range(50):
-            label = QLabel()
-            label.setText(f'Conversation {i + 1}')
-            label.setStyleSheet('background-color: cyan')
-            if i % 2 == 0:
-                label.setAlignment(Qt.AlignRight)
-            history_layout.addWidget(label, i, i % 2, 1, 2)
-            history_layout.setRowMinimumHeight(i,40)
-        history_widget.setLayout(history_layout)
-        self.history_box.setWidget(history_widget)
+        self.history_box.setWidgetResizable(True)
+        self.history_box.setFocusPolicy(Qt.NoFocus)
+        self.history_box.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.question_box = QLineEdit()
 
+        space = 30
+        margin = 5
+        
+        history_layout = QVBoxLayout()
+        history_layout.setContentsMargins(margin, margin, margin, margin)
+        for i in range(50):
+            dialogue = QLabel()
+            dialogue.setWordWrap(True)
+            dialogue.setText(' '.join(['Some Text' for _ in range(i + 1)]))
+            self.history_box.setMinimumWidth(space + 2 * margin +  dialogue.minimumSizeHint().width() + self.history_box.verticalScrollBar().sizeHint().width() + 2 * self.history_box.frameWidth())
+            dialogue_layout = QHBoxLayout()
+            if i % 2 == 0:
+                dialogue.setStyleSheet('background-color: cyan')
+                dialogue_layout.addWidget(dialogue)
+                dialogue_layout.addStretch(1)
+                dialogue_layout.addSpacing(30)
+            else:
+                dialogue.setStyleSheet('background-color: yellow')
+                dialogue_layout.addSpacing(30)
+                dialogue_layout.addStretch(1)
+                dialogue_layout.addWidget(dialogue)
+            history_layout.addLayout(dialogue_layout)
+
+        widget = QWidget()
+        widget.setContentsMargins(0, 0, 0, 0)
+        widget.setLayout(history_layout)
+        self.history_box.setWidget(widget)
+
         layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(self.history_box)
         layout.addWidget(self.question_box)
         self.setLayout(layout)
-    
-    def get_answer(self):
-        return 'Proper answer from ChatPDF'
